ab7bdbb1261cececa13c6818cbc81e473a407bf6
Breyer Hill <Breyer.hill@umconnect.umt.edu>
2017-09-27 18:40:21 -0600

    adding course work p5 examples and commiting hw-3

---
+++ :hw-3/sketch.js
@@ -0,0 +1,17 @@
+// Declare the setup function
+function setup() {
+
+    //create canvas 600px wide and 400px high
+    createCanvas( 900, 500 );
+
+    //color of the background 'pink'
+    background( 'pink' );
+}
+
+
+//declare the draw() function
+function draw(){
+
+  //using text function have p5 write hello world!
+  text( "Hello world!", 450, 250);
+}
