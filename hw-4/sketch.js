d993573434c90b0fe65457395c9ccb419297d7fc
Breyer Hill <Breyer.hill@umconnect.umt.edu>
2017-10-05 00:20:37 -0600

    Homework 4

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -1,12 +1,16 @@
+//background
 function setup() {
     createCanvas( 800, 800 );
     background( 'black' );
 }
+//lumberjack alien
 function draw() {
   translate(400,400)
+  //body
   fill( 'red' )
   noStroke();
     rect( -200, 0, 400, 500, 62);
+    //overalls
   fill( 'yellow' )
     rect( -100, 0, 50, 400);
     rect(   50, 0, 50, 400);
@@ -25,21 +29,26 @@ function draw() {
   fill( 'rgb(16, 229, 16)' )
     rect(-50, -50, 100, 50);
     ellipse(0, -200, 300, 400);
+    //mouth and black triangles for eyes
   fill( 'black')
     arc( 0, -100, 100, 50, 0, 0, HALF_PI );
     triangle( 80, -280, 100, -200, 0, -200);
     triangle( -80, -280, -100, -200, 0, -200);
+    //white triangle part of eye
   fill( 'white')
     triangle( 90, -280, 80, -190, 0, -200);
     triangle( -90, -280, -80, -190, 0, -200);
+    // black circle par of eye
   fill( 'black')
     ellipse(-40,-220,30,30);
     ellipse(40,-220,30,30);
+    //eyebrows
   fill( 'black')
   stroke( 'black')
   strokeWeight(20)
   line(100,-300,10,-220)
   line(-100,-300,-10,-220)
+  arms
   fill ( 'red')
   noStroke();
     quad(-200,50,-100,80,-280,280,-300,200)
